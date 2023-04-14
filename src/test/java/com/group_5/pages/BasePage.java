83350c3c09aba48e90dd62bf5cc26560a215f68d
e1982b <e1982b@gmail.com>
2023-04-14 17:09:09 +0200

    Constructor added

--- src/test/java/com/group_5/pages/BasePage.java
+++ src/test/java/com/group_5/pages/BasePage.java
@@ -1,5 +1,19 @@
 package com.group_5.pages;
 
+import com.group_5.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public abstract class BasePage {
+    public BasePage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(xpath = "//*[.='Kabul Et']")
+    public WebElement understandBtn;
+
+
+
 
 }
