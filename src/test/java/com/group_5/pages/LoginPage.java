2eb622122e70889ea215881947ac54f6caf8546a
mehmet1217 <mehmetaliakbas1@hotmail.com>
2023-04-16 11:59:50 +0200

    Test_2, Test_3, Test_4 Update

---
+++ src/test/java/com/group_5/pages/LoginPage.java
@@ -0,0 +1,41 @@
+package com.group_5.pages;
+
+import com.group_5.utilities.ConfigurationReader;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "login-email")
+    public WebElement emailInput;
+
+    @FindBy(id = "login-password")
+    public WebElement passwordInput;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement girisYapSubmit;
+
+    @FindBy(xpath = "//div[@class='ky-error']")
+    public WebElement errorMessage;
+
+    public void login(){
+        getLoginPage();
+        emailInput.sendKeys(ConfigurationReader.get("username"));
+        passwordInput.sendKeys(ConfigurationReader.get("password"));
+        girisYapSubmit.click();
+        understandBtn.click();
+    }
+
+    public void login(String username, String password){
+        getLoginPage();
+        emailInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        girisYapSubmit.click();
+//      understandBtn.click();
+    }
+
+    public void verifyNOTLogin(){
+        Assert.assertTrue(errorMessage.getText().contains("E-Posta Adresi ya da şifreniz yanlış."));
+    }
+}
--- src/test/java/com/group_5/test/Deneme/deneme.java
+++ src/test/java/com/group_5/test/Deneme/deneme.java
@@ -1,11 +1,62 @@
 package com.group_5.test.Deneme;
 
+import com.group_5.pages.AccountPage;
+import com.group_5.pages.LoginPage;
 import com.group_5.test.TestBase;
+import com.group_5.utilities.BrowserUtils;
 import org.testng.annotations.Test;
 
 public class deneme extends TestBase {
+
+    LoginPage loginPage;
+    AccountPage accountPage;
+
     @Test
     public void test1() {
         System.out.println("Hallo World");
     }
+
+    @Test
+    public void k_Y_LoginWithCorrect_Mail_Pass() {
+        loginPage = new LoginPage();
+        accountPage = new AccountPage();
+        extentLogger = report.createTest("Kitap Yurdu E2E Test");
+        extentLogger.info("Login with correct Mail and Password");
+        loginPage.login();
+        extentLogger.info("Verify that login is successfull");
+        accountPage.verifyLogin();
+        extentLogger.info("PASSED");
+    }
+
+    @Test
+    public void k_Y_NotLogin_Test(){
+        loginPage = new LoginPage();
+        accountPage = new AccountPage();
+        extentLogger = report.createTest("Kitap Yurdu E2E Test");
+        extentLogger.info("Login not with incorrect Mail and Password");
+        loginPage.login("doorfree@gmail.com", "klmn.123");
+        extentLogger.info("Verify that login is not successfull");
+        loginPage.verifyNOTLogin();
+        extentLogger.info("PASSED");
+    }
+
+    @Test
+    public void ky_LogOutUserAndVerify(){
+        loginPage = new LoginPage();
+        accountPage = new AccountPage();
+        extentLogger = report.createTest("Kitap Yurdu E2E Test");
+        extentLogger.info("Login with correct Mail and Password");
+        loginPage.login();
+        extentLogger.info("Verify that login is successfull");
+        accountPage.verifyLogin();
+        extentLogger.info("Click Logout Button");
+        BrowserUtils.hover(accountPage.account);
+        accountPage.cikis.click();
+        extentLogger.info("Verify navigate to Homepage");
+        loginPage.girisYap.isDisplayed();
+        extentLogger.info("PASSED");
+    }
+
+
+
 }
